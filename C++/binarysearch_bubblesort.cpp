64e36618bf11b37dd14161233ab1185c83466b74
JakeTheLion89 <Jake.Leonen@gmail.com>
2016-01-08 11:46:18 -0500

    added binarysearch_bubblesort sample into c++ collection

---
+++ C++/binarysearch_bubblesort.cpp
@@ -0,0 +1,93 @@
+#include "iostream"
+#include "algorithm"
+#include "vector"
+
+using namespace std;
+
+bool binarysearch_function(vector<int> v_sub, int key);
+vector<int> int_bubblesort(vector<int> v_sub);
+
+int main()
+{
+    int ints[10] = {1,2,18,6,7,23,9,12,53,99};
+    int user_input = 0;
+
+    vector<int> intVector (ints, ints + sizeof(ints)/sizeof(int));
+
+    cout << "Type in a number is see if its in the array: ";
+    cin >> user_input;
+
+    cout << binarysearch_function(intVector, user_input) << endl;
+
+    return 0;
+
+};
+
+bool binarysearch_function(vector<int> sub_v, int key){
+
+    // First initiate indexes for hi and lo
+    int lo, hi = 0;
+    vector<int> v = sub_v;
+
+    hi = v.size() - 1;
+
+    // order the vector
+    v = int_bubblesort(v);
+
+    // while lo is less than or equal to hi
+    // derive the middle index
+    // if the key is greater than the middle value of the vector,
+    //      shorten the search range by making hi equal middle index - 1
+    // if the key is less than the middle value
+    //      shorten the search range by making lo equal the middle index + 1
+    // else
+    //      return confirmation that the middle value is equal ot the key
+    // function returns false if the lo is equal or greater than hi
+    //      this means that no element in the vector has the key
+    while (lo <= hi) {
+        int mid = lo + (hi - lo)/2;
+        if (key < v[mid])
+            hi = mid - 1;
+        else if (key > v[mid])
+            lo = mid + 1;
+        else
+            return true;
+    }
+
+    return false;
+
+}
+
+
+vector<int> int_bubblesort(vector<int> sub_v){
+
+    /*
+        Bubble sort
+
+        For the number of elements in the vector
+        For all elements in the vector
+            compare each element to the element ahead of it
+            if the elements are not in order switch them
+    */
+    int i = 0;
+
+    vector<int> v = sub_v;
+
+    for (i=0;i < v.size(); i++){
+
+        int j = 0;
+
+        for(j=0; j < v.size() - 1;j++){
+
+            if(v[j] > v[j+1]){
+
+                int temp = v[j];
+
+                v[j] = v[j+1];
+                v[j+1] = temp;
+            };
+        };
+    };
+
+    return v;
+}
