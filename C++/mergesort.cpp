2e2252714e332e686fb5ab03a801c98207024221
JakeTheLion89 <Jake.Leonen@gmail.com>
2016-01-17 22:48:14 -0500

    done with mergesort

--- C++/mergesort.cpp
+++ C++/mergesort.cpp
@@ -48,10 +48,17 @@ std::vector<int> generate_integers(int range_input, int max){
 }
 
 std::vector<int> merge(std::vector<int> l_vector, std::vector<int> r_vector){
+
+    // after the vector is partitioned to one element per vector
+    // the algo takes two vectors and compares them
     std::vector<int> result;
 
+    //reserveing space on the result vector
     result.reserve(l_vector.size() + r_vector.size());
 
+    // for each element in the left array and right
+    // switch the elements add the greater to to the right
+    // and the lesser to the left
     while ((l_vector.size() > 0) && (r_vector.size() > 0)){
         if (l_vector[0] <= r_vector[0]){
             result.insert(result.end(),l_vector[0]);
@@ -63,6 +70,9 @@ std::vector<int> merge(std::vector<int> l_vector, std::vector<int> r_vector){
         }
     }
 
+
+    // and then append what is left over on the left vector and the right vector
+    // to the end of the array
     if(l_vector.size() > 0){
         result.insert(result.end(),l_vector.begin(), l_vector.end());
     }
@@ -71,15 +81,18 @@ std::vector<int> merge(std::vector<int> l_vector, std::vector<int> r_vector){
         result.insert(result.end(),r_vector.begin(), r_vector.end());
     }
 
-    // test this and explain how it works
+    // return result
     return result;
 }
 
 std::vector<int> td_mergesort(std::vector<int> v){
-
+    // the mergesort works recursively
+    // if v is only one element long
+    // return v
     if (v.size() <= 1)
         return v;
 
+    //then separate into two particions
     std::vector<int> left, right;
 
     for(int i = 0; i < v.size(); i++){
@@ -89,8 +102,13 @@ std::vector<int> td_mergesort(std::vector<int> v){
             right.push_back(v[i]);
     };
 
+    //the merge sort happens recursively
+    // first it will sort elements of the left side
     left = td_mergesort(left);
+
+    // then the right
     right = td_mergesort(right);
 
+    //and return a sorted merged vector
     return merge(left,right);
 }
