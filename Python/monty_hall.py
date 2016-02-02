f9e079e31c7a055042309ea5bc00c9ede0030320
JakeTheLion89 <Jake.Leonen@gmail.com>
2016-02-01 21:46:31 -0500

    Edited monty hall to barebones. Also looks cleaner me thinks

--- Python/monty_hall.py
+++ Python/monty_hall.py
@@ -1,6 +1,6 @@
 import random
 
-def monty_hall(num_rounds,choice_bool):
+def monty_hall(num_rounds,switch_bool):
 
     round_counter = 0
     doors = [True, False, False]
@@ -12,29 +12,33 @@ def monty_hall(num_rounds,choice_bool):
     while (round_counter < num_rounds):
         #first choose a door
         choice = random.choice(doors)
-        #then remove a False door (if you are adding choice)
-        if choice_bool == True:
-            doors_for_2nd_choice = list(doors)
-            doors_for_2nd_choice.pop()
+
+        # monty_hall then removes one of the doors
+        # and invites the player to switch
+
+        #if the player chooses to switch
+        if switch_bool:
             #tell player to switch
-            if choice == True:
+            if choice:
                 choice = False
             else:
                 choice = True
+
+        #else the player stays with the result
 
         # register result
         if choice == True:
             win_counter += 1 # NEW CAR!
         else:
-            loss_counter += 1
+            loss_counter += 1 # A GOAT BLEEEAHHH
 
         round_counter += 1
 
     # Show off calculations
-    if choice_bool == True:
-        print "====Monty Hall with Switch===="
+    if switch_bool:
+        print "\n====Monty Hall with Switch===="
     else:
-        print "====Monty Hall without Switch===="
+        print "\n====Monty Hall without Switch===="
 
     print "Number of Games " + str(num_rounds)
     print "Number of Wins: " + str(win_counter)
