c4be18876de3c1a64b93d6448a8011f34cbb9421
JakeTheLion89 <Jake.Leonen@gmail.com>
2016-04-17 18:38:50 -0400

    cors work 4

--- claims-integrity-system/views/js/dashboard.js
+++ claims-integrity-system/views/js/dashboard.js
@@ -7,5 +7,6 @@ function loadDoc() {
   };
   xhttp.open("POST", "http://ec2-54-87-167-86.compute-1.amazonaws.com:13000/analysts/getAnalystClaims", true);
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.setRequestHeader("Access-Control-Allow-Origin","*");
   xhttp.send("{'analystId':'1'}");
 }
