0566611ef974dd4961c19012fa71cc30a853f684
JakeTheLion89 <Jake.Leonen@gmail.com>
2016-04-17 14:23:51 -0400

    got the html to work. now get those transactions working

--- claims-integrity-system/routes/index.js
+++ claims-integrity-system/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, "../views", "index.html"))
+  res.sendFile(path.join(__dirname + '/../views/index.html'))
   });
 
 module.exports = router;
---
+++ claims-integrity-system/views/js/dashboard.js
@@ -0,0 +1,11 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById("claim-list").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("POST", "http://ec2-54-87-167-86.compute-1.amazonaws.com:13000/analysts/getAnalystClaims", true);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send("{'analystId':'1'}");
+}
