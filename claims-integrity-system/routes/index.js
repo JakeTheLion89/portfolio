d8effb2e9a958962a596a4c2a19ffcdc808c45fe
JakeTheLion89 <Jake.Leonen@gmail.com>
2016-04-19 20:39:17 -0400

    seperated manager and analyst prototypes2

--- claims-integrity-system/routes/index.js
+++ claims-integrity-system/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../views/index.html'))
+  });
+
 router.get('/analystPrototype', function(req, res, next) {
   res.sendFile(path.join(__dirname + '/../views/analystPrototype.html'))
   });
